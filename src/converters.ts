743d9b3bba5322b881cf91caacf4f5fb0345d7c1
hank-swimlane <56449323+hank-swimlane@users.noreply.github.com>
2019-12-19 11:29:28 -0500

    Typo

--- src/converters.ts
+++ src/converters.ts
@@ -107,7 +107,7 @@ export const converters = {
     return x.toLowerCase() === 'true';
   },
   /**
-   * Parse as a string regardless of string contents. Also will URL decode the stirng.
+   * Parse as a string regardless of string contents. Also will URL decode the string.
    *
    * @param str
    * @returns
